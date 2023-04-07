7b7da5b1e8512afec25be80a8143a2fd725096f2
minhlh <minhlh@mor.com.vn>
2023-04-07 11:00:07 +0700

    GET method form

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Template engine
 app.engine("hbs", hbs.engine({ extname: ".hbs" }));
@@ -19,7 +19,16 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 app.get("/news", (req, res) => {
+  console.log("news query", req.query);
   res.render("news");
+});
+app.get("/search", (req, res) => {
+  console.log(123, req.query, req.query.q);
+  res.render("search");
+});
+app.post("/search", (req, res) => {
+  console.log(123, req.query, req.query.q);
+  res.render("search");
 });
 
 app.listen(port, () => {
